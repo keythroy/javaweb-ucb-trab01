2c8747b4e6fb3207392d20b5afb3061ef27911a5
Keyth Rodrigues <keyth.roy.rodrigues@accenture.com>
2020-11-14 10:40:34 -0300

    project spring maven web mysql

---
+++ src/main/java/ucb/keyth/imoveis/WillRealStateApplication.java
@@ -0,0 +1,13 @@
+package ucb.keyth.imoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillRealStateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillRealStateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucb/keyth/imoveis/WillRealStateApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.keyth.imoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillRealStateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
